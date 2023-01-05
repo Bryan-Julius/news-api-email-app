f9625190b2aa51d4099a5be6ad6acf4447bab23f
Bryan <Bryanjulius77@gmail.com>
2023-01-05 11:26:55 -0500

    added links,specify english,limit news,add subject,control category (getting 401)

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import requests
 from Send_Email import send_email
 
+topic = "tesla"
+
 api_key = "1524468cf3f9418ebe9c31a9b0a4fcc7"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2022-12-04&sortBy=publishedAt&apiKey=" \
-      "1524468cf3f9418ebe9c31a9b0a4fcc7"
+url = "https://newsapi.org/v2/everything?" \
+        f"q={topic}&" \
+        "sortBy=publishedAt&" \
+        "apiKey=1524468cf3f9418ebe9c31a9b0a4fcc7" \
+        "language=en"
 # Make request
 request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
 
-body = ""
 
 # Access the article titles and description
-for article in content["articles"]:
+body = ""
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = "Subject: Today's news" + "\n" + body + article["title"] + "\n" \
+                + article["description"] \
+                + "\n" + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
-
